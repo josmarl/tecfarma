d0a00f00510dc9c7931127c82c527e336dc80ee5
jose manuel limachi chavez <josmarl@bitbucket.org>
2016-02-02 19:17:41 -0500

    nuevo cambio

--- cryptoUtil/src/test/java/crypto/util/crypto/test/CrytoTest.java
+++ cryptoUtil/src/test/java/crypto/util/crypto/test/CrytoTest.java
@@ -12,7 +12,6 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import pe.gob.onpe.eva.security.EvaKeys;
 
 /**
  *
@@ -42,7 +41,7 @@ public class CrytoTest {
     @Test
     public void Encriptar() throws CryptoException {
 
-        Crypto crypto = Crypto.getInstance(EvaKeys.ENCRYPT);
+        Crypto crypto = Crypto.getInstance("");
         String data = "jjdbc:oracle:thin:EVA_VECINAL/EVA_VECINAL@192.168.49.149:1521:BDD3S4";
         String dataEncrypt = crypto.encrypt(data);
         System.out.println(dataEncrypt);
@@ -52,7 +51,7 @@ public class CrytoTest {
 
     public static void main(String[] args) throws CryptoException {
 
-        Crypto crypto = Crypto.getInstance(EvaKeys.ENCRYPT);
+        Crypto crypto = Crypto.getInstance("");
 //        String data = "[{\"id\":2,\"idCarga\":59,\"scope\":\"J30001\",\"scopeAlias\":\"LIMA\",\"type\":2,\"stAlias\":\"ODPE\",\"numSort\":0,\"address\":null,\"timeBegin\":\"18:29:19\",\"timeClose\":\"18:29:19\",\"phone\":null,\"status\":1,\"numElec\":0,\"idEleccion\":1,\"eleccion\":\"ELECCIONES MUNICIPALES 2015\",\"eleccionDesc\":\"ELECCIONES MUNICIPALES 2015\",\"puestaCero\":38,\"proceso\":\"ELECCIONES MUNICIPALES 2015\",\"webService\":\"http://172.16.91.74/evaWebService/\",\"audDateMod\":0,\"audUserMod\":22,\"centroComputo\":\"C30001\"}]";
 //        String dataEncrypt = crypto.encrypt(data);
 //        System.out.println(dataEncrypt);
